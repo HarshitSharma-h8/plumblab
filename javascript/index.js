0600d51f884aa7c195e54f53b8c829a0b74bd285
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-11-05 15:27:50 +0530

    add navbar

---
+++ javascript/index.js
@@ -0,0 +1,45 @@
+let toggle = false;
+// target element
+const moboiconAll = document.querySelectorAll(".menuToggle");
+console.log(moboiconAll);
+
+const mobomenu = document.getElementById("mobileMenu");
+
+moboiconAll.forEach((moboicon) => {
+  moboicon.addEventListener("click", (e) => {
+    if (!toggle) {
+      mobomenu.style.transform = "translateX(0)";
+      toggle = true;
+    } else {
+      mobomenu.style.transform = "translateX(-500px)";
+      toggle = false;
+      const dropSymbole = document.getElementById("dropMenu");
+      dropSymbole.classList.remove("rotate-180");
+      const dropdown = document.getElementById("mobileDropdown");
+      dropdown.classList.add("hidden");
+    }
+  });
+});
+
+// Close the menu if clicking outside of it
+document.addEventListener("click", (e) => {
+  if (
+    toggle &&
+    !mobomenu.contains(e.target) &&
+    !e.target.closest(".menuToggle")
+  ) {
+    mobomenu.style.transform = "translateX(-500px)";
+    toggle = false;
+    const dropSymbole = document.getElementById("dropMenu");
+    dropSymbole.classList.remove("rotate-180");
+    const dropdown = document.getElementById("mobileDropdown");
+    dropdown.classList.add("hidden");
+  }
+});
+
+function toggleDropdown() {
+  const dropdown = document.getElementById("mobileDropdown");
+  const dropSymbole = document.getElementById("dropMenu");
+  dropSymbole.classList.toggle("rotate-180");
+  dropdown.classList.toggle("hidden");
+}
