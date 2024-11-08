8904bac9db6d596799449c873564349895e5ded0
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-11-08 15:39:55 +0530

    complete home page

--- javascript/index.js
+++ javascript/index.js
@@ -11,7 +11,7 @@ moboiconAll.forEach((moboicon) => {
       mobomenu.style.transform = "translateX(0)";
       toggle = true;
     } else {
-      mobomenu.style.transform = "translateX(-500px)";
+      mobomenu.style.transform = "translateX(-600px)";
       toggle = false;
       const dropSymbole = document.getElementById("dropMenu");
       dropSymbole.classList.remove("rotate-180");
@@ -28,7 +28,7 @@ document.addEventListener("click", (e) => {
     !mobomenu.contains(e.target) &&
     !e.target.closest(".menuToggle")
   ) {
-    mobomenu.style.transform = "translateX(-500px)";
+    mobomenu.style.transform = "translateX(-600px)";
     toggle = false;
     const dropSymbole = document.getElementById("dropMenu");
     dropSymbole.classList.remove("rotate-180");
@@ -57,7 +57,7 @@ new Swiper('.slider-wrapper', {
   loop: true,
   autoplay: {
     delay: 1000,
-    disableOnInteraction: true,
+    disableOnInteraction: false,
   },
   grabCursor: true,
   spaceBetween: 30,
@@ -91,4 +91,143 @@ new Swiper('.slider-wrapper', {
 
 
 
-// stats js
+// image gallary
+
+function reset(){
+  const gasline = document.querySelectorAll('.gasline')
+  gasline.forEach(e => e.classList.remove("hidden"))
+  const kitchen = document.querySelectorAll('.kitchen')
+  kitchen.forEach(e => e.classList.remove("hidden"))
+  const waterline = document.querySelectorAll('.waterline')
+  waterline.forEach(e => e.classList.remove("hidden"))
+  const bathroom = document.querySelectorAll('.bathroom')
+  bathroom.forEach(e => e.classList.remove("hidden"))
+}
+
+const allimg = document.getElementById("Allimg")
+const Bathroom = document.getElementById("Bathroom")
+const Gasline = document.getElementById("Gasline")
+const Kitchen = document.getElementById("Kitchen")
+const Waterline = document.getElementById("Waterline")
+allimg.addEventListener('click',function(){
+  reset()
+  Bathroom.classList.remove("isActive")
+  Gasline.classList.remove("isActive")
+  Kitchen.classList.remove("isActive")
+  Waterline.classList.remove("isActive")
+  allimg.classList.add("isActive")
+})
+
+
+Bathroom.addEventListener('click',function(){
+  reset()
+  const gasline = document.querySelectorAll('.gasline')
+  gasline.forEach(e => e.classList.add("hidden"))
+  const kitchen = document.querySelectorAll('.kitchen')
+  kitchen.forEach(e => e.classList.add("hidden"))
+  const waterline = document.querySelectorAll('.waterline')
+  waterline.forEach(e => e.classList.add("hidden"))
+  Bathroom.classList.add("isActive")
+  Gasline.classList.remove("isActive")
+  Kitchen.classList.remove("isActive")
+  Waterline.classList.remove("isActive")
+  allimg.classList.remove("isActive")
+})
+
+Gasline.addEventListener('click',function(){
+  reset()
+  const bathroom = document.querySelectorAll('.bathroom')
+  bathroom.forEach(e => e.classList.add("hidden"))
+  const kitchen = document.querySelectorAll('.kitchen')
+  kitchen.forEach(e => e.classList.add("hidden"))
+  const waterline = document.querySelectorAll('.waterline')
+  waterline.forEach(e => e.classList.add("hidden"))
+  Bathroom.classList.remove("isActive")
+  Gasline.classList.add("isActive")
+  Kitchen.classList.remove("isActive")
+  Waterline.classList.remove("isActive")
+  allimg.classList.remove("isActive")
+})
+
+Kitchen.addEventListener('click',function(){
+  reset()
+  const kitchen = document.querySelectorAll('.kitchen')
+  kitchen.forEach(e => e.classList.remove("hidden"))
+  const bathroom = document.querySelectorAll('.bathroom')
+  bathroom.forEach(e => e.classList.add("hidden"))
+  const gasline = document.querySelectorAll('.gasline')
+  gasline.forEach(e => e.classList.add("hidden"))
+  const waterline = document.querySelectorAll('.waterline')
+  waterline.forEach(e => e.classList.add("hidden"))
+  Bathroom.classList.remove("isActive")
+  Gasline.classList.remove("isActive")
+  Kitchen.classList.add("isActive")
+  Waterline.classList.remove("isActive")
+  allimg.classList.remove("isActive")
+})
+
+Waterline.addEventListener('click',function(){
+  reset()
+  const kitchen = document.querySelectorAll('.kitchen')
+  kitchen.forEach(e => e.classList.add("hidden"))
+  const bathroom = document.querySelectorAll('.bathroom')
+  bathroom.forEach(e => e.classList.add("hidden"))
+  const gasline = document.querySelectorAll('.gasline')
+  gasline.forEach(e => e.classList.add("hidden"))
+  const waterline = document.querySelectorAll('.waterline')
+  waterline.forEach(e => e.classList.remove("hidden"))
+  Bathroom.classList.remove("isActive")
+  Gasline.classList.remove("isActive")
+  Kitchen.classList.remove("isActive")
+  Waterline.classList.add("isActive")
+  allimg.classList.remove("isActive")
+})
+
+
+// take me top
+// Show the button when scrolled down 100px
+window.onscroll = function () {
+  const backToTopButton = document.getElementById("backToTop");
+  if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+    backToTopButton.classList.remove("hidden");
+    backToTopButton.classList.add("opacity-100");
+  } else {
+    backToTopButton.classList.add("hidden");
+    backToTopButton.classList.remove("opacity-100");
+  }
+};
+
+// Scroll to top function
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+// navbar
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar")
+window.addEventListener("scroll",function(){
+  const scrollTop = window.pageYOffset || window.scrollY || documentElement.scrollTop
+  if(!toggle){if(scrollTop > lastScrollTop){
+    navbar.style.top = "-100px"
+  }else{
+    navbar.style.top = "0"
+  }}
+  lastScrollTop = scrollTop
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
